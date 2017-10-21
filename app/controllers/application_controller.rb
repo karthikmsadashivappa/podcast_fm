48353e31b810400803b8a82cb9f22572d6f0d4b3
Karthik M S <ms18karthik@gmail.com>
2017-10-22 00:15:09 +0530

    Added attributes to the devise podcast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	  	  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :title])
+
+  	      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay])
+
+  end
+
 end
